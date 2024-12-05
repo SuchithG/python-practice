60d43c1d1d9db424aa2b1beda41b68dff860d7be
SuchithG <suchithgowda1995@gmail.com>
2024-12-05 12:51:49 +0530

    Lists

---
+++ Lists/Modifying Lists/adding_removing_lists.py
@@ -0,0 +1,15 @@
+items = ["a", "b", "c", "d", "e"]
+
+# Adding elements
+items.append("g")
+print(items)
+
+items.insert(1, "a.1")
+print(items)
+
+items1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Removing elements
+items1.pop(-1)
+print(items1)
+items1.remove(8)
+print(items1)
---
+++ Lists/Modifying Lists/slicing_lists.py
@@ -0,0 +1,10 @@
+l = [100, 200, 300, 400, 500]
+# Indexing
+print(l[0:3])
+
+l2 = l[1:3]
+print(l2)
+
+
+# printing alternate elements in a list
+print(l[0::2])
