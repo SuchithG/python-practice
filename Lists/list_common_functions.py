8e35ba6e381a7efb9bc99050f526868b16d446f6
SuchithG <suchithgowda1995@gmail.com>
2024-12-05 13:23:31 +0530

    some common list functions

---
+++ Lists/list_common_functions.py
@@ -0,0 +1,8 @@
+l = ["one", "two", "three", "four", "five", "six", "seven", "eight"]
+l2= [1,3,4,6,7,10,9,12,11]
+
+print(len(l)) # Returns the number of elements in the list.
+
+print(sorted(l2)) # Returns a new sorted list without changing the original list
+
+print(sum(l2)) # Returns the sum of elements in a list (for numerical lists)
