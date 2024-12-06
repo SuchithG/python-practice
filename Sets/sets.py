e5a17b862439c4fd9ca56cd2aa5461da84c4bf75
SuchithG <suchithgowda1995@gmail.com>
2024-12-07 00:18:30 +0530

    Declaring a set

--- Sets/sets.py
+++ Sets/sets.py
@@ -1,3 +1,11 @@
-s = {1,2,3,4,5,6,7,8,9}
+# Declaring a set
+s = {1,2,3,4,5,6,7,8,9} # using "{ }"
 
-print(type(s))
+s1 = set((1,2,3)) # using set function 
+#s1 = set([(1,2,3),("Python",),(3,)])  # The set() function expects a single iterable, like a list, tuple, or string, from which it will construct the set.
+
+# Declaring empty set
+s2 = set()
+
+print(type(s2))
+
