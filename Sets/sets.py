53770a64fc152d1adea77142135ef49a5dfc143a
SuchithG <suchithgowda1995@gmail.com>
2024-12-07 00:28:30 +0530

    Some set operations

--- Sets/sets.py
+++ Sets/sets.py
@@ -1,4 +1,4 @@
-# Declaring a set
+#------Declaring a set-------#
 s = {1,2,3,4,5,6,7,8,9} # using "{ }"
 
 s1 = set((1,2,3)) # using set function 
@@ -9,3 +9,27 @@ s2 = set()
 
 print(type(s2))
 
+
+#-----Set Operations------#
+# Union
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+print(set1 | set2) # Output: {1, 2, 3, 4, 5}
+
+# Intersection
+intersection_set = set1 & set2
+print(intersection_set) # Output: {3}
+
+# Difference
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+result = A - B  # Elements in A but not in B
+print(result)
+
+# Symmetric Difference ( ^ )
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+result = A ^ B  # Elements in A or B, but not both
+print(result)
