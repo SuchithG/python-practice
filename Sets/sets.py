68ea5201a5f1cf0e8e78766392e7ce9f10ebb9f6
SuchithG <suchithgowda1995@gmail.com>
2024-12-07 00:47:19 +0530

    Commonly ued methods in sets

--- Sets/sets.py
+++ Sets/sets.py
@@ -33,3 +33,27 @@ B = {3, 4, 5, 6}
 
 result = A ^ B  # Elements in A or B, but not both
 print(result)
+
+
+#----Set Methods------#
+
+# Add
+fruits_set = {"apple", "banana", "cherry"}
+fruits_set.add("orange") # Adds an element to the set
+print(fruits_set)  # Output: {'orange', 'cherry', 'apple', 'banana'} <--- unordered
+
+# Remove 
+fruits_set.remove("banana") # Removes a specified element from the set. Raises an error if the element does not exist.
+print(fruits_set) # Output: {'orange', 'cherry', 'apple'}  <--- unordered
+
+# Discard
+fruits_set.discard("banana") # Removes a specified element without raising an error if it does not exist.
+print(fruits_set) # Output: {'apple', 'orange', 'cherry'} <--- No error if "banana" is not in the set
+
+# Pop
+fruits_set.pop() # Removes a random element from the set.
+print(fruits_set) # Output: {'orange', 'cherry'}
+
+#clear
+fruits_set.clear() # Removes all elements from the set.
+print(fruits_set) # Output : set()
