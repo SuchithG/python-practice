72affa892a24bccb4032a3cc71553ec49a66c616
SuchithG <suchithgowda1995@gmail.com>
2024-12-07 20:19:36 +0530

    Creating dictionary

---
+++ Dictionary/dictionary.py
@@ -0,0 +1,19 @@
+# Creating a dictionary
+# Syntax
+# You can create a dictionary using curly braces {} or the dict() function
+my_dict = {
+    "key1": "value1", # Key value pairs
+    "key2": "value2",
+    "key3": "value3"
+}
+
+# using the dict() function
+new_dict = dict(Suchith="Male", Shashank="Male")
+print(new_dict)
+
+# Example 2 : Let's create a dictionary of famous cities in Karnataka and their popular dishes
+karnataka_food = {
+    "Bengaluru": "Bisi Bele Bath",
+    "Mysuru": "Mysore Pak",
+    "Mangaluru": "Neer Dosa"
+}
