1169d1bca3174f0afef1d81b7a54903e0bffd5bb
SuchithG <suchithgowda1995@gmail.com>
2024-12-09 00:37:49 +0530

    feat:

---
+++ Loops/while_loops.py
@@ -0,0 +1,15 @@
+# example 1: print numbers from 1 to 5 using a while loop
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+
+# example 2: Imagine you're counting sheep to fall asleep
+
+sheep_count = 1
+
+while sheep_count <= 10:
+    print(f"Sheet {sheep_count}")
+    sheep_count += 1
