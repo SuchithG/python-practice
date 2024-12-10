dbd8bf0150b07e962659959b2457d9a5961cc524
SuchithG <suchithgowda1995@gmail.com>
2024-12-10 19:18:20 +0530

    feat:

--- Loops/while_loops.py
+++ Loops/while_loops.py
@@ -1,3 +1,16 @@
+# While loop
+
+is_failed = True
+i = 1
+
+while is_failed:
+    if i%2!=0:
+           continue
+    print(f"Try {i}")
+    i = i + 1   
+    if i > 100:
+        break
+
 # example 1: print numbers from 1 to 5 using a while loop
 
 i = 1
@@ -13,3 +26,8 @@ sheep_count = 1
 while sheep_count <= 10:
     print(f"Sheet {sheep_count}")
     sheep_count += 1
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
