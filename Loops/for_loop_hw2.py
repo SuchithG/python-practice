9a8f39d6ff89f6decd3b159257b45d5c8f9f410f
SuchithG <suchithgowda1995@gmail.com>
2024-12-16 14:04:12 +0530

    Looping through dictionaries

--- Loops/for_loop_hw2.py
+++ Loops/for_loop_hw2.py
@@ -25,3 +25,25 @@ foods = ["Dosa", "Idli", "Vada", "Bisibelebath"]
 
 for food in foods:
     print(f"I like {food}")
+
+
+# Looping Through Dictionaries
+
+# Iterating over dictionary keys
+student_marks = {"Anand": 85, "Geetha": 90, "Kumar": 78}
+
+for student in student_marks:
+    print(f"{student}")
+
+# Iterating over dictionary values
+student_marks = {"Anand": 85, "Geetha": 90, "Kumar": 78}
+
+for marks in student_marks.values():
+    print(marks)
+
+# Iterating over both keys and values
+student_marks = {"Anand": 85, "Geetha": 90, "Kumar": 78}
+
+for student, marks in student_marks.items():
+    print(f"{student} scored {marks} marks")
+
