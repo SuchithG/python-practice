e803f7c82dc04080a4aebde94655973f2bba80a3
SuchithG <suchithgowda1995@gmail.com>
2024-12-19 23:50:53 +0530

    prob: 3

--- comprehensions/comprehensions_hw.py
+++ comprehensions/comprehensions_hw.py
@@ -8,7 +8,6 @@ uppercase_food_names = [ up_fd_nme.upper() for up_fd_nme in kannada_foods]
 print(uppercase_food_names)
 
 #Sum of Prices:
-
 #Create a dictionary of 5 items with their prices. Write a program that calculates the total price of all items using a for loop.
 
 items_prices = {
@@ -18,11 +17,15 @@ items_prices = {
     "items_4": 8,
     "items_5": 10,
 }
-
-#total_prices = (price.sum() for items, price in items_prices.values())
 sum_prices = 0
 
 for value in items_prices.values():
     sum_prices += value
     
 print(sum_prices)
+
+#List of Squares:
+#Create a list of numbers from 1 to 10. Use list comprehension to generate a list of their squares.
+
+squares = [ num**2 for num in range(1, 11)]
+print(squares)
