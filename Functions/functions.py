e619dda0d24f03be3f30b69b35757a02a641b8dc
SuchithG <suchithgowda1995@gmail.com>
2024-12-20 19:38:07 +0530

    functions - syntax and examples

---
+++ Functions/functions.py
@@ -0,0 +1,29 @@
+# Functions
+# Basic synatx
+def function_name(parameters):
+    # Block of code
+    print("")
+
+
+# Basic function to greet a user
+def greet():
+     print("Hello users!")
+greet()
+
+
+# Ex: 1    
+def marriage(boy, girl): #parameters
+    print(f"{boy} married {girl}")
+
+marriage("John", "Selina") #positional parameters
+
+marriage(boy="Johnny", girl="clay") #keyword parameters
+
+
+# Ex: 2
+def tables(num):
+    for i in range(1,11):
+            print(f"{num} X {i} = {num*i}")
+
+
+tables(1)
