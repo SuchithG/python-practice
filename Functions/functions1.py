4e1b840c05b9bd7e656fbe3778ee36f2162a4b0a
SuchithG <suchithgowda1995@gmail.com>
2024-12-21 15:59:34 +0530

    Variable-length arguments

--- Functions/functions1.py
+++ Functions/functions1.py
@@ -5,3 +5,12 @@ def display_info(name, age):
     print(f"Name: {name}, Age: {age}")
 
 display_info(age=25, name="Kumar")
+
+# Variable-Length arguments
+# You can use *args and **kwargs to accept a variable number of arguments in a function.
+# Example: Using *args
+
+def total_sum(*numbers):
+    return sum(numbers)
+
+print(total_sum(1, 2, 3, 4, 5))
