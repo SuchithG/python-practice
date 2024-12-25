b22127eee997c1fbd4d7953106e4c795c347deb1
SuchithG <suchithgowda1995@gmail.com>
2024-12-25 13:29:22 +0530

    Recursive function Example 3 Example 2 : logic correction in function

--- Functions/recursive_functions_hw.py
+++ Functions/recursive_functions_hw.py
@@ -16,6 +16,20 @@ def factorial_of_numbers(n):
     if n == 1:
         return 1
     else:
-        return n * (n - 1)
+        return n * factorial_of_numbers(n - 1)
     
 print(factorial_of_numbers(3))
+
+# Example 3:
+# Write a recursive function to find the n-th Fibonacci number
+def fib_of_numbers(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib_of_numbers(n - 1) + fib_of_numbers(n - 2)
+
+print( fib_of_numbers(3))( fib_of_numbers(3))
+
+
