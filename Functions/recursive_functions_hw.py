ddd931d8118c2910144b904a3fc222d992f0fbdc
SuchithG <suchithgowda1995@gmail.com>
2024-12-25 13:13:44 +0530

    Recursive function Example 2

--- Functions/recursive_functions_hw.py
+++ Functions/recursive_functions_hw.py
@@ -8,3 +8,14 @@ def sum_of_numbers(n):
         return n + sum_of_numbers(n - 1)
 
 print(sum_of_numbers(3))
+
+# Example 2:
+# Write a recursive function to calculate the factorial of a number n
+
+def factorial_of_numbers(n):
+    if n == 1:
+        return 1
+    else:
+        return n * (n - 1)
+    
+print(factorial_of_numbers(3))
