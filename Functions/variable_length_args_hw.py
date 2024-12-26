c1d4ec18751453a78cd4de3b2a90f6f4925a21f4
SuchithG <suchithgowda1995@gmail.com>
2024-12-26 23:13:18 +0530

    Variable-Length Arguments Example 2

--- Functions/variable_length_args_hw.py
+++ Functions/variable_length_args_hw.py
@@ -1,9 +1,17 @@
 # Variable-Length Arguments
 # Example 1
 # Write a function that accepts any number of arguments and returns their average
-
 def average(*n):
     return sum(n) / len(n)
 
 print(f"{average(10, 30 , 28):.2f}")
+
+# Example 2
+# Sum of Squares
+def Sum_of_Squares(*args):
+    return sum(x**2 for x in args)
+
+print(f"{Sum_of_Squares(1, 2, 3):.2f}")
+
+
 
